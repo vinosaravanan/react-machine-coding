f27f00506f16d5cdbfb43fe2ca4a6d3e8d4b6e74
vinosaravanan <152616485+vinosaravanan@users.noreply.github.com>
2026-06-30 17:20:07 +0530

    create  project

---
+++ fetch-api-context-api/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react';
+
+function App() {
+
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <section id="center" className='text-red-600 '>
+        vvvvvvvvvvvvvvvvvvvxxxxxxx
+      </section>
+    </>
+  )
+}
+
+export default App
